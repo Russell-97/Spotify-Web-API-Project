4fc8a27161f0db55ecc6659487c152c566256054
russ05 <russ05@students.cs.ubc.ca>
2025-08-18 20:01:41 +0530

    added login page

--- main.py
+++ main.py
@@ -4,48 +4,57 @@ from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+# Load .env variables
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
-# Spotify config
+# Spotify configuration
 SPOTIPY_CLIENT_ID = os.getenv("CLIENT_ID")
 SPOTIPY_CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 SPOTIPY_REDIRECT_URI = os.getenv("REDIRECT_URI")
-SCOPE = "user-read-private user-read-email user-library-read playlist-read-private user-top-read"
+SCOPE = "user-read-private user-read-email user-top-read"
 
-@app.route('/')
-def login():
-    sp_oauth = SpotifyOAuth(
+# OAuth setup
+def create_spotify_oauth():
+    return SpotifyOAuth(
         client_id=SPOTIPY_CLIENT_ID,
         client_secret=SPOTIPY_CLIENT_SECRET,
         redirect_uri=SPOTIPY_REDIRECT_URI,
         scope=SCOPE
     )
+
+@app.route('/')
+def index():
+    return render_template("login.html")
+
+@app.route('/login')
+def login():
+    sp_oauth = create_spotify_oauth()
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
 
 @app.route('/callback')
 def callback():
-    sp_oauth = SpotifyOAuth(
-        client_id=SPOTIPY_CLIENT_ID,
-        client_secret=SPOTIPY_CLIENT_SECRET,
-        redirect_uri=SPOTIPY_REDIRECT_URI,
-        scope=SCOPE
-    )
+    sp_oauth = create_spotify_oauth()
+    session.clear()
     code = request.args.get('code')
     token_info = sp_oauth.get_access_token(code)
     session["token_info"] = token_info
-    print(url_for('profile'))
     return redirect(url_for('profile'))
 
 @app.route('/profile')
 def profile():
-    token_info = session.get("token_info")
+    token_info = session.get("token_info", None)
     if not token_info:
-        return redirect(url_for('/'))
-    
+        return redirect(url_for('login'))
+
+    if token_info['expires_at'] - 10 < int(spotipy.oauth2.time.time()):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+        session['token_info'] = token_info
+
     sp = spotipy.Spotify(auth=token_info['access_token'])
     user = sp.current_user()
     top_artists = sp.current_user_top_artists(limit=10, time_range='short_term')
@@ -53,5 +62,10 @@ def profile():
 
     return render_template('index.html', user=user, artists=top_artists['items'], tracks=top_tracks['items'])
 
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 80)))
+    app.run(debug=True)
